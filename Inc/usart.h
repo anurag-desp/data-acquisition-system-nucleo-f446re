ae49eac248f4c607aa90f9f15637298565132195
Aayush Ashok Kashyap <aak.ayush001@gmail.com>
2024-07-30 15:21:28 +0530

    Refactor and document USART

--- Inc/usart.h
+++ Inc/usart.h
@@ -15,43 +15,68 @@
 #include "stm32f446xx.h" // Include STM32F446xx specific definitions
 #include "gpio.h"
 
+// ANSI escape codes for terminal control and text colors
 #define clearScreen() printf("\033[H\033[J")
-#define KNRM  "\x1B[0m"
-#define KRED  "\x1B[91m"
-#define KGRN  "\x1B[92m"
-#define KYEL  "\x1B[93m"
-#define KBLU  "\x1B[94m"
-#define KMAG  "\x1B[95m"
-#define KCYN  "\x1B[96m"
-#define KWHT  "\x1B[97m"
+#define KNRM  "\x1B[0m"   // Normal (default) color
+#define KRED  "\x1B[91m"  // Red
+#define KGRN  "\x1B[92m"  // Green
+#define KYEL  "\x1B[93m"  // Yellow
+#define KBLU  "\x1B[94m"  // Blue
+#define KMAG  "\x1B[95m"  // Magenta
+#define KCYN  "\x1B[96m"  // Cyan
+#define KWHT  "\x1B[97m"  // White
 
 #define MAX_COLOR_CODE_LENGTH 8
-
 #define MAX_INPUT_SIZE (0xFFFFFFFF)
 #define NEWLINE_CHARACTER '\n'
 #define NULL_CHARACTER '\0'
 
-#define USART2_CR1_M_WORD_LENGTH_8				((uint8_t) 0)
-#define USART2_CR1_M_WORD_LENGTH_9				((uint8_t) 1)
+// USART2 configuration constants
+#define USART2_CR1_M_WORD_LENGTH_8    ((uint8_t) 0)
+#define USART2_CR1_M_WORD_LENGTH_9    ((uint8_t) 1)
 
+// Function prototypes for UART2 operations
 
-
+/**
+ * @brief Initialize UART2
+ */
 extern void UART2_init(void);
 
+/**
+ * @brief Clear USART2 CR1 register
+ */
 extern void USART2_clear_CR1(void);
 
+/**
+ * @brief Enable USART2
+ */
 extern void USART2_enable(void);
 
+/**
+ * @brief Set USART2 word length
+ * @param length Word length (8 or 9 bits)
+ */
 extern void USART2_set_word_length(uint8_t length);
 
+/**
+ * @brief Enable USART2 transmitter
+ */
 extern void USART2_enable_transmitter(void);
 
+/**
+ * @brief Enable USART2 receiver
+ */
 extern void USART2_enable_receiver(void);
 
+/**
+ * @brief Set default baud rate for USART2
+ */
 extern void USART2_set_default_baud_rate(void);
 
+/**
+ * @brief Quick default configuration for USART2
+ */
 extern void USART2_quick_default_config(void);
-
 
 /**
  * @brief Sends a single character over UART2
@@ -73,7 +98,8 @@ uint8_t UART2_getchar(void);
 
 /**
  * @brief Receives a string from UART2
- * @param passwd Array to store the received string
+ * @param string Array to store the received string
+ * @param input_size Maximum size of the input string
  */
 void UART2_getString(char string[], uint32_t input_size);
 
--- Src/usart.c
+++ Src/usart.c
@@ -10,11 +10,17 @@
 
 #include "usart.h"
 
-
+/**
+ * @brief Initialize UART2 and configure related GPIO pins
+ *
+ * This function enables the USART2 clock, initializes and configures PA2 and PA3
+ * for alternate function (UART), and sets up the basic UART2 configuration.
+ */
 void UART2_init(void) {
     // Enable USART2 clock
     RCC->APB1ENR |= RCC_APB1ENR_USART2EN;
 
+    // Initialize and configure GPIO pins for UART2
     GPIOx_init(PA);
     GPIOx_config_mode(PA2, MODER_AF);
     GPIOx_config_mode(PA3, MODER_AF);
@@ -32,42 +38,66 @@ void UART2_init(void) {
     USART2->CR1 |= (1 << 3); // Enable transmitter
 }
 
-
+/**
+ * @brief Clear USART2 CR1 register
+ *
+ * This function resets the USART2 CR1 register to its default state.
+ */
 void USART2_clear_CR1(void) {
     USART2->CR1 = 0x00;
 }
 
-
+/**
+ * @brief Enable USART2
+ *
+ * This function enables the USART2 peripheral.
+ */
 void USART2_enable(void) {
     USART2->CR1 |= USART_CR1_UE;
 }
 
-
+/**
+ * @brief Set USART2 word length
+ * @param length Word length (8 or 9 bits)
+ *
+ * This function sets the word length for USART2 communication.
+ */
 void USART2_set_word_length(uint8_t length) {
-	if (length != USART2_CR1_M_WORD_LENGTH_9)
-		USART2->CR1 &= ~USART_CR1_M;
-	else
-		USART2->CR1 |=  USART_CR1_M;
+    if (length != USART2_CR1_M_WORD_LENGTH_9)
+        USART2->CR1 &= ~USART_CR1_M;
+    else
+        USART2->CR1 |=  USART_CR1_M;
 }
 
-
+/**
+ * @brief Enable USART2 transmitter
+ *
+ * This function enables the transmitter for USART2.
+ */
 void USART2_enable_transmitter(void) {
     USART2->CR1 |= (1 << PA2);
 }
 
-
+/**
+ * @brief Enable USART2 receiver
+ *
+ * This function enables the receiver for USART2.
+ */
 void USART2_enable_receiver(void) {
     USART2->CR1 |= (1 << PA3);
 }
 
-
+/**
+ * @brief Set default baud rate for USART2
+ *
+ * This function sets the default baud rate (115200) for USART2.
+ */
 void USART2_set_default_baud_rate(void) {
     USART2->BRR = (7 << 0) | (24 << 4);
 }
 
-
 /**
- * @brief Configures UART2 for communication
+ * @brief Quick default configuration for USART2
  *
  * This function sets up UART2 with the following configuration:
  * - Baud rate: 115200
@@ -77,19 +107,20 @@ void USART2_set_default_baud_rate(void) {
  * - Flow control: None
  */
 void USART2_quick_default_config(void) {
-	UART2_init();
-	USART2_clear_CR1();
-	USART2_enable();
-	USART2_set_word_length(USART2_CR1_M_WORD_LENGTH_8);
-	USART2_set_default_baud_rate();
-	USART2_enable_transmitter();
-	USART2_enable_receiver();
+    UART2_init();
+    USART2_clear_CR1();
+    USART2_enable();
+    USART2_set_word_length(USART2_CR1_M_WORD_LENGTH_8);
+    USART2_set_default_baud_rate();
+    USART2_enable_transmitter();
+    USART2_enable_receiver();
 }
 
-
 /**
- * @brief Sends a single character over UART2
+ * @brief Send a single character over UART2
  * @param c The character to send
+ * 
+ * This function sends a single character over UART2 and waits for the transmission to complete.
  */
 void UART2_sendChar(uint32_t c) {
     USART2->DR = c; // Load the data into the data register
@@ -97,8 +128,10 @@ void UART2_sendChar(uint32_t c) {
 }
 
 /**
- * @brief Sends a string over UART2
+ * @brief Send a string over UART2
  * @param string The null-terminated string to send
+ *
+ * This function sends a null-terminated string over UART2.
  */
 void UART2_sendString(char *string) {
     while ((*string) != '\0') {
@@ -108,8 +141,10 @@ void UART2_sendString(char *string) {
 }
 
 /**
- * @brief Receives a single character from UART2
+ * @brief Receive a single character from UART2
  * @return The received character
+ *
+ * This function waits for a character to be received on UART2 and returns it.
  */
 uint8_t UART2_getchar(void) {
     while (!(USART2->SR & (1 << 5))); // Wait until data is received
@@ -117,17 +152,22 @@ uint8_t UART2_getchar(void) {
 }
 
 /**
- * @brief Receives a string from UART2
- * @param passwd Array to store the received string
+ * @brief Receive a string from UART2
+ * @param string Array to store the received string
+ * @param input_size Maximum size of the input string
+ *
+ * This function receives a string from UART2, echoes back each character,
+ * and terminates the string when a newline character is received or the
+ * maximum input size is reached.
  */
 void UART2_getString(char string[], uint32_t input_size) {
-    uint8_t len = input_size; // Maximum password size
+    uint8_t len = input_size; // Maximum input size
     for (int i = 0; i < len-1; i++) {
         string[i] = UART2_getchar();
         if (string[i] == NEWLINE_CHARACTER) {
             UART2_sendString("\n\r");
-        	string[i] = NULL_CHARACTER;
-        	return;
+            string[i] = NULL_CHARACTER;
+            return;
         }
         UART2_sendChar(string[i]); // Echo back the received character
     }
